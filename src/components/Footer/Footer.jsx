aa1532f17e28cf6acb7fdfd2bda134970803bdd9
amanmi625 <aman9063mishra@gmail.com>
2025-12-22 21:12:45 +0530

    Resolve merge conflict in Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -140,19 +140,8 @@ export default function Footer() {
       </div>
 
       <div className="text-center border-t border-deepForest mt-10 pt-4 text-sm font-poppins">
-<<<<<<< HEAD
         © 2025 Stonora. All rights reserved. | <span>Privacy Policy</span> |{" "}
-        <span>Terms of Service</span>
-=======
-        © 2025 Stonora. All rights reserved. |{" "}
-        <Link to="/privacy-policy" className="hover:underline">
-          Privacy Policy
-        </Link>{" "}
-        |{" "}
-        <Link to="/terms-of-service" className="hover:underline">
-          Terms of Service
-        </Link>
->>>>>>> 06a68d34d04f3e291e65c49f8b2adccbe2dcb823
+        <span>Terms of Service</span>{" "}
       </div>
     </footer>
   );
